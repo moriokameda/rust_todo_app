32d5aad072895b336a9627b145f090b402853acf
moriokameda <mkame19941031@gmail.com>
2023-05-10 21:54:19 +0900

    add error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
-use actix_web::{App, get, HttpResponse, HttpServer};
+use actix_web::{App, get, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
+use thiserror::Error;
+
+struct TodoEntity {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntity>,
+}
+
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("failed to render HTML")]
+    AskamaError(#[from] askama::Error)
+}
+
+impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello World";
-    Ok(HttpResponse::Ok().body(response_body))
+async fn index() -> Result<HttpResponse, MyError> {
+    let mut entries = Vec::new();
+    entries.push(TodoEntity {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntity {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+    let html = IndexTemplate { entries };
+
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
